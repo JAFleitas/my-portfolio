7ab1f28c758b21d1c92b69f121d5d729fe931e67
JAFleitas <psi.gonzalo.fleitas@gmail.com>
2022-04-14 20:16:53 -0300

    modificando el type de globalStyle

--- styles/globalStyles/globalStyles.ts
+++ styles/globalStyles/globalStyles.ts
@@ -1,10 +1,11 @@
 import { createGlobalStyle } from "styled-components";
+import { ColorPallet } from "../../models/colorPallet";
 
-export const GlobalStyles = createGlobalStyle`
+export const GlobalStyles = createGlobalStyle<{ theme: ColorPallet }>`
   body {
     transition: all ${(props) => props.theme.transitionTime} ;
     background:${({ theme }) => theme.background} ;
-     
+     filter: blur(4px);
   }
 
 `;
