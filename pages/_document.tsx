85f50ed8368d64393572561b54969d764334edda
JAFleitas <psi.gonzalo.fleitas@gmail.com>
2022-04-14 18:39:22 -0300

    solucionando error de eslint

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ import { ServerStyleSheet } from "styled-components";
 export default class MyDocument extends Document {
   static async getInitialProps(
     ctx: DocumentContext
-  ): Promise<DocumentInitialProps> {
+  ): Promise<DocumentInitialProps | any> {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
 
