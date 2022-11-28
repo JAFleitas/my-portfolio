5560d2e7fb288463172650f8619c748566b4301a
Gonzalo Fleitas <psi.gonzalo.fleitas@gmail.com>
2022-11-28 19:03:44 -0300

    editando componente boton principal

--- components/ui/PrimaryButton/styles.ts
+++ components/ui/PrimaryButton/styles.ts
@@ -1,15 +1,14 @@
-import styled, { keyframes } from "styled-components";
-import { MOBILE, TABLET } from "../../../helpers/constants";
+import styled from "styled-components";
+import { MOBILE } from "../../../helpers/constants";
 
 export const ButtonStyled = styled.button`
   display: flex;
   min-width: 180px;
-  /* border: 4px solid #234ca7; */
+
   border: none;
   border-radius: 12px;
   border-left: 1px solid #333;
 
-  /*  background: #161b22; */
   background-color: transparent;
   color: #ccc;
   text-align: center;
@@ -29,89 +28,6 @@ export const ButtonStyled = styled.button`
   }
 `;
 
-/* const roundedTurn = keyframes
-
-   0%{
-    background: linear-gradient(40deg, #0d1118 60%, #404c5e);
-  
-  }
-   5%{
-    background: linear-gradient(58deg, #0d1118 60%, #404c5e);
-  
-  }
-   10%{
-    background: linear-gradient(76deg, #0d1118 60%, #404c5e);
-  
-  }
-   15%{
-    background: linear-gradient(94deg, #0d1118 60%, #404c5e);
-  
-  }
-   20%{
-    background: linear-gradient(112deg, #0d1118 60%, #404c5e);
-  
-  }
-   25%{
-    background: linear-gradient(130deg, #0d1118 60%, #404c5e);
-  
-  } 30%{
-    background: linear-gradient(148deg, #0d1118 60%, #404c5e);
-  
-  } 35%{
-    background: linear-gradient(166deg, #0d1118 55%, #404c5e);
-  
-  } 
-  
-  40%{
-    background: linear-gradient(184deg, #0d1118 50%, #404c5e);
-  
-  } 45%{
-    background: linear-gradient(202deg, #0d1118 60%, #404c5e);
-  
-  }
-   50%{
-    background: linear-gradient(220deg, #0d1118 60%, #6b80a0);
-  
-  }
-   55%{
-    background: linear-gradient(238deg, #0d1118 60%, #6b80a0);
-  
-  }
-   60%{
-    background: linear-gradient(256deg, #0d1118 60%, #6b80a0);
-  
-  }
-   65%{
-    background: linear-gradient(274deg, #0d1118 60%, #6b80a0);
-  
-  }
-   70%{
-    background: linear-gradient(292deg, #0d1118 60%, #6b80a0);
-  
-  }
-   75%{
-    background: linear-gradient(310deg, #0d1118 60%, #6b80a0);
-  
-  }
-   80%{
-    background: linear-gradient(328deg, #0d1118 60%, #6b80a0);
-  
-  } 85%{
-    background: linear-gradient(346deg, #0d1118 55%, #404c5e);
-  
-  } 90%{
-    background: linear-gradient(364deg, #0d1118 50%, #404c5e);
-  
-  } 95%{
-    background: linear-gradient(382deg, #0d1118 60%, #404c5e);
-  
-  } 100%{
-    background: linear-gradient(400deg, #0d1118 60%, #404c5e);
-  
-  }
-   
-
-*/
 export const Container = styled.div`
   display: flex;
   justify-content: center;
