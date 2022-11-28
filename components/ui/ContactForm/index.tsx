1f99d1132897ef17f5fd88eeb02c4e92630adb3b
Gonzalo Fleitas <psi.gonzalo.fleitas@gmail.com>
2022-11-28 12:53:08 -0300

    agregando funcionalidad al formulario de contacto con nodemailer

--- components/ui/ContactForm/index.tsx
+++ components/ui/ContactForm/index.tsx
@@ -1,52 +1,62 @@
-import { SubTitle } from "../../../styles/globalStyledComponents";
 import { useState } from "react";
+import axios from "axios";
 import { Box, Grid, TextField, Button, Chip, Divider } from "@mui/material";
-/* import { useForm } from "react-hook-form"; */
+import styled from "styled-components";
 
-/* import { LinkComponent } from "../../components/ui";
-import { validations } from "../../utils"; */
-import { ErrorOutline } from "@mui/icons-material";
+import { SubTitle } from "../../../styles/globalStyledComponents";
+import { SocialNetworkContainer } from "../DescriptionProfile";
+
+import { Linkedin } from "../icons/Linkedin";
+import { Twitter } from "../icons/Twitter";
+import { GitHubIcon } from "../icons/Github";
+
+import { useForm } from "react-hook-form";
+
+import { ErrorOutline, ThumbUpRounded } from "@mui/icons-material";
+
+import { isEmail } from "../../../helpers/validations";
 
 type FormData = {
   email: string;
-  password: string;
+  text: string;
 };
 export const ContactForm = () => {
-  /* const {
-      register,
-      handleSubmit,
-      formState: { errors },
-    } = useForm<FormData>();
- */
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+
   const [showError, setShowError] = useState(false);
+  const [showOkey, setShowOkey] = useState(false);
+  const [emailInput, setEmailInput] = useState("");
+  const [textInput, setTextInput] = useState("");
 
-  const onLoginUser = async ({ email, password }: FormData) => {
-    /* const isValidLogin = await loginUser(email, password);
+  const sendEmail = async ({ email, text }: FormData) => {
+    const isSend = await axios.post("/api/contactme", {
+      from: email,
+      text,
+    });
 
-      if (!isValidLogin) {
-        setShowError(true);
-        setTimeout(() => setShowError(false), 3000);
-        return;
-      }
-
-      router.replace("/"); */
+    if (!isSend) {
+      setShowError(true);
+      setTimeout(() => setShowError(false), 3000);
+      return;
+    }
+    setEmailInput("");
+    setTextInput("");
+    setShowOkey(true);
+    setTimeout(() => setShowOkey(false), 2000);
+    return;
   };
   return (
     <section id="contact">
       <Container>
         <SubTitle>Contacto</SubTitle>
-        <form /* onSubmit={handleSubmit(onLoginUser)} */ noValidate>
+        <form onSubmit={handleSubmit(sendEmail)} noValidate>
           <Box sx={{ width: 350, padding: "10px 20px" }}>
             <Grid container spacing={4}>
-              <Grid
-                item
-                xs={12}
-                display="flex"
-                flexDirection="column"
-                alignItems="center"
-              ></Grid>
-
-              <Grid item xs={12}>
+              {/*   <Grid item xs={12}>
                 <TextField
                   autoFocus
                   color="secondary"
@@ -55,7 +65,7 @@ export const ContactForm = () => {
                   fullWidth
                   inputRef={(input) => input?.focus()}
 
-                  /* {...register("password", {
+                  {...register("password", {
                     required: "Required field",
                     minLength: {
                       value: 6,
@@ -63,20 +73,23 @@ export const ContactForm = () => {
                     },
                   })}
                   error={!!errors.password}
-                  helperText={errors.password?.message} */
+                  helperText={errors.password?.message}
                 />
-              </Grid>
+              </Grid> */}
               <Grid item xs={12}>
                 <TextField
                   type="email"
                   label="Email"
                   fullWidth
-                  /* {...register("email", {
+                  autoFocus={true}
+                  {...register("email", {
                     required: "Required field",
-                    validate: validations.isEmail,
+                    validate: isEmail,
                   })}
                   error={!!errors.email}
-                  helperText={errors.email?.message} */
+                  helperText={errors.email?.message}
+                  value={emailInput}
+                  onChange={(e) => setEmailInput(e.target.value)}
                 />
               </Grid>
               <Grid item xs={12}>
@@ -87,15 +100,17 @@ export const ContactForm = () => {
                   fullWidth
                   multiline
                   rows={4}
-                  /* {...register("password", {
+                  {...register("text", {
                     required: "Required field",
                     minLength: {
-                      value: 6,
-                      message: "Password requires min 6 characters",
+                      value: 10,
+                      message: "se requiere un texto de 10 caracteres",
                     },
                   })}
-                  error={!!errors.password}
-                  helperText={errors.password?.message} */
+                  error={!!errors.text}
+                  helperText={errors.text?.message}
+                  value={textInput}
+                  onChange={(e) => setTextInput(e.target.value)}
                 />
               </Grid>
               <Grid item xs={12}>
@@ -113,15 +128,23 @@ export const ContactForm = () => {
               <Grid
                 item
                 xs={12}
-                sx={{ minHeight: 60 }}
+                sx={{ minHeight: 80, marginBottom: "6px" }}
                 display="flex"
                 justifyContent="center"
               >
                 {showError ? (
                   <Chip
-                    label="Credentials error"
+                    label="Ocurrió un error"
                     color="error"
                     icon={<ErrorOutline />}
+                    className="fadeIn"
+                  />
+                ) : null}
+                {showOkey ? (
+                  <Chip
+                    label="Su correo fue enviado, Muchas gracias!"
+                    color="success"
+                    icon={<ThumbUpRounded />}
                     className="fadeIn"
                   />
                 ) : null}
@@ -130,21 +153,15 @@ export const ContactForm = () => {
           </Box>
         </form>
       </Container>
-      <Divider role="presentation">Or</Divider>
+      <Divider role="presentation">Redes sociales</Divider>
       <SocialNetworkContainer>
-        <Linkedin fillHover="#1293d2" fill="#0f89e3" />
+        <Linkedin fill="#0a66c2" fillHover="#0a66da" />
         <GitHubIcon />
         <Twitter fill="#0f89e3" fillHover="#33c5ff" />
       </SocialNetworkContainer>
     </section>
   );
 };
-
-import styled from "styled-components";
-import { SocialNetworkContainer } from "../DescriptionProfile";
-import { Linkedin } from "../icons/Linkedin";
-import { Twitter } from "../icons/Twitter";
-import { GitHubIcon } from "../icons/Github";
 
 const Container = styled.article`
   display: flex;
